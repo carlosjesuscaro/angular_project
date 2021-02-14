74c4016e4d6174cc96ad50ecfabee8b36b7e87a3
carloscaro <carlosjesuscaro@icloud.com>
2021-02-13 20:21:28 -0500

    success and warning alerts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: darkred;
+    }
+
+    body {
+      background-color: lightgreen;
+    }
+  `]
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    TestComponentComponent,
+    WarningAlertComponent,
+    SuccesAlertComponent
   ],
   imports: [
     BrowserModule
