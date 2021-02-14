6d0705a0b0f9818a31fb62850f37b8af788c9056
carloscaro <carlosjesuscaro@icloud.com>
2021-02-14 00:08:33 -0500

    adding interaction

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl:'./server.component.html'
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
 
+
+  // tslint:disable-next-line:typedef
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
