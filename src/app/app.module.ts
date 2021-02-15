3f4e027eb76c08f798bbf3c4f95dacf8240b16db
carloscaro <carlosjesuscaro@icloud.com>
2021-02-15 17:52:51 -0500

    working with Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { NgClass} from '@angular/common';
+import { NgStyle} from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,17 +7,25 @@ import { FormsModule } from '@angular/forms';
 /*    <app-server></app-server>*/
 /*    <app-server></app-server>`,*/
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  /*styleUrls: ['./servers.component.css']*/
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   isText = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'No name for now';
+  serverStatus = 'offline';
+  servers = ['TestServer 1', 'TestServer 2', 'TestServer 3', 'TestServer 4']
 
   /*Initializing method from the export class*/
   constructor() {
     setTimeout(() => {this.allowNewServer = true; }, 2000 );
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
 
   ngOnInit(): void {
@@ -28,5 +36,10 @@ export class ServersComponent implements OnInit {
   onCreateServer(){
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
     this.isText = true;
+  }
+
+  // tslint:disable-next-line:typedef
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
