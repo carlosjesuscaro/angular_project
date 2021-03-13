ed4705a3d9b8bd45f537b017daf7ab2908fd4029
Carlos Caro <carlosjesuscaro@icloud.com>
2021-03-13 18:22:27 -0500

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
